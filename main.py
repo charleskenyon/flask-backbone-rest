7a98da4c259e46558f53ffa551c9828b2fd4f85d
Charles Kenyon <rory.kenyon01@gmail.com>
2015-05-21 10:59:18 +0000

    updating...

--- main.py
+++ main.py
@@ -1,22 +1,93 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort, request
 import json
 import sqlite3
+import dataset
 app = Flask(__name__)
+
+def get_db_table():
+	db = dataset.connect('sqlite:///posts.sqlite')
+	table = db['posts_table']
+	
+	return table
+
+def create_and_update_posts():
+	counter = 0
+	posts = []
+
+	for post in table:
+		each_post = {
+			'id': counter,
+			'Name': post['Name'],
+			'Email': post['Email'],
+			'Text': post['Text'],
+			'Date': post['Date']
+		}
+		counter += 1
+		
+		posts.append(each_post)
+		
+	for post in posts:
+		table.update(post, ['Name', 'Email', 'Text', 'Date'])
+	
+	return posts
+	
+table = get_db_table()
+posts = create_and_update_posts()
 
 @app.route('/')
 def main_page():
 	return render_template('index.html')
 
-@app.route('/posts')
-def create_json():
-	connection = sqlite3.connect("posts.sqlite")
-	curs = connection.cursor()
-	
-	curs.execute('SELECT * FROM posts_table')
-	posts = [dict(Name=row[0], Text=row[1], Date=row[2], Email=row[3]) for row in curs.fetchall()]
-	connection.close()
-	
+@app.route('/posts', methods=['GET'])
+def create_json_posts():
 	return json.dumps(posts)
+
+@app.route('/posts/<int:post_id>', methods=['GET'])
+def create_json_post(post_id):
+	get_post = table.find_one(id=post_id)
+	
+	if get_post is None:
+		abort(404)
+	
+	return json.dumps(posts[post_id])
+		
+@app.route('/posts', methods=['POST'])
+def add_new_json_post():
+	if not request.json:
+		abort(404)
+	
+	new_post = {
+		'id': posts[-1]['id'] + 1,
+		'Name': request.json['Name'],
+		'Email': request.json['Email'],
+		'Text': request.json['Text'],
+		'Date': request.json['Date']
+	}
+	
+	posts.append(new_post)
+	table.insert(new_post)
+	
+	return json.dumps(posts[-1]), 201
+	
+@app.route('/posts/<int:post_id>', methods=['DELETE'])
+def delete_post(post_id):
+	remove_post = table.find_one(id=post_id)
+	
+	if remove_post is None:
+		abort(404)
+		
+	remove_post = {
+		'id': remove_post['id'],
+		'Name': remove_post['Name'],
+		'Email': remove_post['Email'],
+		'Text': remove_post['Text'],
+		'Date': remove_post['Date']
+	}
+	
+	posts.remove(remove_post)
+	table.delete(id=post_id)
+	
+	return json.dumps(posts[post_id])
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,5 @@
+var counter = 0;
+
 var Post = Backbone.Model.extend({
 	
 	urlRoot: "/posts"
@@ -18,9 +20,11 @@ var Posts = Backbone.Collection.extend({
 			var newDate = splitDate[1] + '/' + splitDate[0] + '/' + splitDate[2];
 			var delta = Date.parse(newDate);
 			delta = delta * -1;
-			model.set({"timeDelta": delta})
+			model.set({"timeDelta": delta});
 		});
 		this.sort();
+		
+		return this;
 	}
 	
 });
@@ -31,7 +35,22 @@ var PostView = Backbone.View.extend({
 		class: "row"
 	},
 	
+	events: {
+		"click #delete": "onClickDelete"
+	},
+	
+	onClickDelete: function(event){
+		event.preventDefault();
+		this.model.destroy();
+	},
+	
 	render: function(){
+		if (this.model.attributes.id == 0) {
+			this.$el.attr("id", this.model.id);
+		} else {
+			this.$el.attr("id", this.model.id || counter);
+		};
+		counter++;
 		var template = _.template($("#postTemplate").html());
 		var html = template(this.model.toJSON());
 		this.$el.html(html);
@@ -43,10 +62,66 @@ var PostView = Backbone.View.extend({
 
 var PostsView = Backbone.View.extend({
 	
-	el: "#container1",
+	el: "body",
 	
 	initialize: function() {
 		this.render();
+		
+		this.model.on("add", this.onAddPost, this);
+		this.model.on("remove", this.onRemovePost, this);
+	},
+	
+	events: {
+		"click #postButton": "onClickAdd"
+	},
+	
+	onClickAdd: function() {
+		var $name = $("#name")
+		var $email = $("#email")
+		var $text = $("#comment")
+		var todaysDate = this.getDate();
+		
+		var newPost = new Post({Name: $name.val(), Email: $email.val(), Date: todaysDate, Text: $text.val()});
+		
+		if (newPost.attributes.Name && newPost.attributes.Email && newPost.attributes.Text) {
+			this.model.add(newPost);
+			newPost.save();
+			$name.val(""); 
+			$email.val(""); 
+			$text.val("");
+		} else {
+			alert("Please fill out all the required fields...");
+		};
+	},
+	
+	getDate: function() {
+		var today = new Date();
+		var dd = today.getDate();
+		var mm = today.getMonth() + 1; 
+
+		var yy = today.getFullYear();
+		yy = yy.toString();
+		yy = yy.substring(2, 4);
+		
+		if (dd < 10) {
+			dd = '0' + dd
+		};
+		if (mm < 10) {
+			mm = '0' + mm
+		}; 
+		
+		var today = dd + '/' + mm + '/' + yy;
+		
+		return today;
+	},
+	
+	onAddPost: function(post) {
+		var newPostView = new PostView({model: post});
+		$("#container1").prepend(newPostView.render().$el);
+	},
+	
+	onRemovePost: function(removedPost){
+		this.$("div #" + removedPost.id).remove();
 	},
 	
 	render: function(){
@@ -54,7 +129,7 @@ var PostsView = Backbone.View.extend({
 		
 		this.model.each(function(post){
 			var postView = new PostView({model: post});
-			self.$el.append(postView.render().$el);
+			$("#container1").append(postView.render().$el);
 		});
 	}
 
@@ -69,6 +144,6 @@ posts.fetch({
 });
 
 var successCallback = function(){
-	posts.orderByDate()
+	posts.orderByDate();
 	var postsView = new PostsView({model: posts})
 };
