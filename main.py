e10e1395ceb9baa19697e6be614ec6ff6987a5df
Charles Kenyon <rory.kenyon01@gmail.com>
2015-06-03 12:16:46 +0000

    updating...

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-from flask import Flask, render_template, abort, request
+from flask import Flask, render_template, abort, request, g, Response
 import json
 import sqlite3
 import dataset
 app = Flask(__name__)
 
-def get_db_table():
-	db = dataset.connect('sqlite:///posts.sqlite')
-	table = db['posts_table']
-	
-	return table
-
 def create_and_update_posts():
 	posts = []
 
-	for post in table:
+	for post in g.table:
 		each_post = {
 			'id': post['id'],
 			'Name': post['Name'],
@@ -25,9 +19,12 @@ def create_and_update_posts():
 		posts.append(each_post)
 	
 	return posts
-	
-table = get_db_table()
-posts = create_and_update_posts()
+
+@app.before_request
+def before_request():
+	g.db = dataset.connect('sqlite:///posts.sqlite')
+	g.table = g.db['posts_table']
+	g.posts = create_and_update_posts()
 
 @app.route('/')
 def main_page():
@@ -35,11 +32,11 @@ def main_page():
 
 @app.route('/posts', methods=['GET'])
 def create_json_posts():
-	return json.dumps(posts)
+	return Response(json.dumps(g.posts), mimetype='application/json')
 
 @app.route('/posts/<int:post_id>', methods=['GET'])
 def create_json_post(post_id):
-	get_post = table.find_one(id=post_id)
+	get_post = g.table.find_one(id=post_id)
 	
 	if get_post is None:
 		abort(404)
@@ -52,7 +49,7 @@ def create_json_post(post_id):
 		'Date': get_post['Date']
 	}
 	
-	return json.dumps(get_post)
+	return Response(json.dumps(get_post), mimetype='application/json')
 		
 @app.route('/posts', methods=['POST'])
 def add_new_json_post():
@@ -60,21 +57,21 @@ def add_new_json_post():
 		abort(404)
 	
 	new_post = {
-		'id': posts[-1]['id'] + 1,
+		'id': g.posts[-1]['id'] + 1,
 		'Name': request.json['Name'],
 		'Email': request.json['Email'],
 		'Text': request.json['Text'],
 		'Date': request.json['Date']
 	}
 	
-	posts.append(new_post)
-	table.insert(new_post)
+	g.posts.append(new_post)
+	g.table.insert(new_post)
 	
-	return json.dumps(posts[-1]), 201
+	return Response(json.dumps(g.posts[-1]), 201, mimetype='application/json')
 	
 @app.route('/posts/<int:post_id>', methods=['DELETE'])
 def delete_post(post_id):
-	remove_post = table.find_one(id=post_id)
+	remove_post = g.table.find_one(id=post_id)
 	
 	if remove_post is None:
 		abort(404)
@@ -87,10 +84,10 @@ def delete_post(post_id):
 		'Date': remove_post['Date']
 	}
 	
-	posts.remove(remove_post)
-	table.delete(id=post_id)
+	g.posts.remove(remove_post)
+	g.table.delete(id=post_id)
 	
-	return json.dumps(posts)
+	return Response(json.dumps(g.posts), mimetype='application/json')
 
 if __name__ == '__main__':
 	app.run(debug=True)
